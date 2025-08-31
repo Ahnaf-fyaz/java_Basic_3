e6161f42b442e968a40641d20f3003228dea09c3
Ahnaf-fyaz <110236486+Ahnaf-fyaz@users.noreply.github.com>
2025-08-31 15:09:12 +0600

    Create Finding_a_Character_in_a_String.java

---
+++ Finding_a_Character_in_a_String.java
@@ -0,0 +1,6 @@
+public class Finding_a_Character_in_a_String {
+  public static void main(String[] args) {
+    String txt = "Please locate where 'locate' occurs!";
+    System.out.println(txt.indexOf("locate"));
+  }
+}

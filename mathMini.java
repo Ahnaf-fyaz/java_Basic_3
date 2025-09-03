f75e992ef9f39e16834cae92adb1ab95f63c18c0
Ahnaf-fyaz <110236486+Ahnaf-fyaz@users.noreply.github.com>
2025-09-04 03:15:55 +0600

    Update mathMini.java

--- mathMini.java
+++ mathMini.java
@@ -1,5 +1,5 @@
-public class mathMini {
+public class Main {
   public static void main(String[] args) {
-    System.out.println(Math.max(5, 10));  
+    System.out.println(Math.min(5, 10));  
   }
 }

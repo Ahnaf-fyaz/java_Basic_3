b9bdc1e1b3ce34410eebc3088dd84e2c9e683ef8
Ahnaf-fyaz <110236486+Ahnaf-fyaz@users.noreply.github.com>
2025-09-04 03:20:00 +0600

    Create mathPow.java

---
+++ mathPow.java
@@ -0,0 +1,5 @@
+public class mathPow  {
+  public static void main(String[] args) {
+    System.out.println(Math.pow(2, 8));
+  }
+}                                                                                                             
